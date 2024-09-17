93d78a0d4f2af92bf4cc01a3873c36eff06af0a4
diego-rg <diego_thegarden@hotmail.com>
2024-09-17 15:17:55 +0200

    project structure

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "Next 14 course",
@@ -9,7 +11,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
