5eaa20250dfa5fd997c421ab7835009668ec326c
diego-rg <diego_thegarden@hotmail.com>
2024-09-17 17:30:37 +0200

    navbar and media queries

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,7 +1,11 @@
+"use client";
+
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
+import { useState } from "react";
 
 export default function Links() {
+    const [open, setOpen] = useState(false);
     const session = true;
     const isAdmin = true;
 
@@ -25,10 +29,27 @@ export default function Links() {
     ];
 
     return (
-        <div className={styles.links}>
-            {links.map((link) => (
-                <NavLink item={link} key={link.title} />
-            ))}
+        <div className={styles.container}>
+            <div className={styles.links}>
+                {links.map((link) => (
+                    <NavLink item={link} key={link.title} />
+                ))}
+                {session ? (
+                    <>
+                        {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+                        <button className={styles.logout}>Logout</button>
+                    </>
+                ) : (
+                    <NavLink item={{ title: "Login", path: "/login" }} />
+                )}
+            </div>
+            <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>menu</button>
+            {open && <div className={styles.mobileLinks}>
+                {links.map((link) => (
+                    <NavLink item={link} key={link.title} />
+                ))}
+            </div>
+            }
         </div>
     );
 };
