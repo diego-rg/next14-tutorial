e26b0d7f80fb61974316b1d82fdf3f7b11dbbcbb
diego-rg <diego_thegarden@hotmail.com>
2024-09-17 16:35:22 +0200

    css variables

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Next 14 course",
@@ -10,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Navbar />
         {children}
         <Footer />
